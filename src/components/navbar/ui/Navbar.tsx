64b5ae3417854bcfe432c5f523dd3d0b290e47a6
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-06 19:32:58 +0300

    add logo

--- src/components/navbar/ui/Navbar.tsx
+++ src/components/navbar/ui/Navbar.tsx
@@ -1,15 +1,18 @@
-import {Button, Flex, Icon} from '@chakra-ui/react';
+import {Button, Flex, Icon, Image} from '@chakra-ui/react';
 import {ReactElement} from 'react';
 import {IoIosCreate} from 'react-icons/io';
 import {NavLink} from 'react-router-dom';
 import {getRouteCreateTask} from '@/app/providers/AppRouter/routeConfig';
 import {BlurBox} from '@/shared/ui';
+import logo from '../../../../public/logoFull.jpg';
 
 export const Navbar = (): ReactElement => {
   return (
     <BlurBox as="header" position="fixed" w="100%" padding={3} zIndex="navbar">
       <Flex gap={5} justifyContent="space-between" alignItems="center">
-        <NavLink to={'/'}>Logo</NavLink>
+        <NavLink to={'/'}>
+          <Image src={logo} w="100px" h="55px" />
+        </NavLink>
         <Button as={NavLink} to={getRouteCreateTask()} variant={'primary'}>
           <Icon boxSize={5} as={IoIosCreate} /> Add new task
         </Button>
