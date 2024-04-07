20c37198ba349ba9d0aa7c52f89a8aa0dda11756
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-07 13:11:38 +0300

    Update Alert.tsx

--- src/shared/ui/alert/Alert.tsx
+++ src/shared/ui/alert/Alert.tsx
@@ -1,6 +1,10 @@
 import 'react-toastify/dist/ReactToastify.css';
+import {useColorMode} from '@chakra-ui/system';
+import {ReactElement} from 'react';
 import {ToastContainer} from 'react-toastify';
 
-export const Alert = () => {
-  return <ToastContainer autoClose={2000} position="top-center" theme="dark" />;
+export const Alert = (): ReactElement => {
+  const theme = useColorMode();
+
+  return <ToastContainer autoClose={2000} position="top-center" theme={theme.colorMode} />;
 };
