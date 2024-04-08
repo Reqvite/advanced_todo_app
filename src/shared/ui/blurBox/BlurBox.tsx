8f2c07b57d2be1076e1590035b9fd3253745dc67
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-08 16:29:26 +0300

    add blurBox animation

--- src/shared/ui/blurBox/BlurBox.tsx
+++ src/shared/ui/blurBox/BlurBox.tsx
@@ -1,16 +1,29 @@
 import {Box, BoxProps, useColorModeValue} from '@chakra-ui/react';
+import {AnimatePresence, motion} from 'framer-motion';
 import {ReactElement, ReactNode} from 'react';
 
 type Props = BoxProps & {
   children: ReactNode;
 };
 
+const MotionBox = motion(Box);
+
 export const BlurBox = ({children, ...otherProps}: Props): ReactElement => {
   const boxBg = useColorModeValue('secondaryBgColorLightTransparent', 'secondaryBgColorDarkTransparent');
 
   return (
-    <Box padding={5} borderRadius="lg" bg={boxBg} css={{backdropFilter: 'blur(8px)'}} {...otherProps}>
-      {children}
-    </Box>
+    <AnimatePresence>
+      <MotionBox
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        padding={5}
+        borderRadius="lg"
+        bg={boxBg}
+        css={{backdropFilter: 'blur(8px)'}}
+        {...otherProps}
+      >
+        {children}
+      </MotionBox>
+    </AnimatePresence>
   );
 };
