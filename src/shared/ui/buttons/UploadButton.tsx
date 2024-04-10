0c859b8e5773ed69db82f3d1aa1f180bb69f027c
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-04-10 20:40:49 +0300

    Update UploadButton.tsx

--- src/shared/ui/buttons/UploadButton.tsx
+++ src/shared/ui/buttons/UploadButton.tsx
@@ -3,7 +3,7 @@ import {ChangeEvent, ReactElement, useState} from 'react';
 import {MdFileUpload} from 'react-icons/md';
 import {uploadTaskSchema} from '@/shared/lib/yup/uploadTask.schema';
 import {useUploadTasksMutation} from '@/slices/task/task.rtk';
-import {ConfirmModal, Input} from '..';
+import {ConfirmModal as Modal, Input} from '..';
 
 export const UploadButton = (): ReactElement => {
   const {isOpen, onOpen, onClose} = useDisclosure();
@@ -37,9 +37,9 @@ export const UploadButton = (): ReactElement => {
     <>
       <IconButton variant="primary" aria-label="Upload" icon={<MdFileUpload />} onClick={onOpen} />
       {isOpen && (
-        <ConfirmModal title="Upload tasks" isOpen={isOpen} onConfirm={onConfirm} onClose={onClose} isDisabled={Boolean(error) || !file}>
+        <Modal title="Upload tasks" isOpen={isOpen} onConfirm={onConfirm} onClose={onClose} isDisabled={Boolean(error) || !file}>
           <Input h="50px" padding="10px" error={error} variant="primary" type="file" onChange={handleFileChange} />
-        </ConfirmModal>
+        </Modal>
       )}
     </>
   );
